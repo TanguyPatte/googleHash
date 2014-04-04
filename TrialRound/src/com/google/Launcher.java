2ec0ac4644f30236f4fba6bfbe05d3e0a2ead13a
Tanguy PATTE <tanguy.patte@gmail.com>
2014-04-04 21:25:21 +0200

    read input file

--- TrialRound/src/com/google/Launcher.java
+++ TrialRound/src/com/google/Launcher.java
@@ -1,5 +1,9 @@
 package com.google;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Launcher {
 
 	/**
@@ -7,7 +11,41 @@ public class Launcher {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BufferedReader br = null;
+		 
+		try {
+ 
+			String sCurrentLine;
+			boolean image[][] ;
+ 
+			br = new BufferedReader(new FileReader("doodle.txt"));
+			String firstLine = sCurrentLine = br.readLine();
+			String dimension[] = firstLine.split(" ");
 
+			int nbLines =  Integer.parseInt(dimension[0]);
+			int nbColumns =  Integer.parseInt(dimension[1]);
+			image = new boolean[nbLines][nbColumns];
+			int lineIterator = 0;
+			while ((sCurrentLine = br.readLine()) != null) {
+			//	System.out.println(sCurrentLine);
+				image[lineIterator] = new boolean[nbColumns];
+				for(int i = 0 ; i< nbColumns ; i++)
+				{
+
+					image[lineIterator][i] = sCurrentLine.charAt(i) == '#' ?	 false :true; 					
+				}
+				lineIterator ++;
+			}
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 
 }
