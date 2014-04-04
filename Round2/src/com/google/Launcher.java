d8264e0fb4f098036ce68d796fd31f2bfb6de213
sam <samuel.humeau@epfl.ch>
2014-04-05 00:44:16 +0200

    launcher

--- Round2/src/com/google/Launcher.java
+++ Round2/src/com/google/Launcher.java
@@ -1,13 +1,22 @@
 package com.google;
 
+import java.util.LinkedList;
+
 public class Launcher {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LinkedList<String> b= new LinkedList<String>();
+		b.add("blabla");
+		b.add("blibli");
+		IOSimple.writeFile("coucou2.txt", b);
+		IOSimple.writeFile("coucou.txt", "on est les champions");
 		System.out.println("Ah que coucou!");
+		for (String s : IOSimple.readFile("coucou3.txt")){
+			System.out.println(s);
+		}
 	}
 
 }
