f8e71c846a4b6a75f5be844c4a84687c6dd9f4ff
sam <samuel.humeau@epfl.ch>
2014-04-05 00:27:45 +0200

    basicio

---
+++ Round2/src/com/google/IOSimple.java
@@ -0,0 +1,33 @@
+package com.google;
+
+import java.util.List;
+
+public class IOSimple {
+	/**
+	 * Return a list of all lines in the file
+	 * @param path
+	 * @return
+	 */
+	List<String> readFile(String path){
+		return null;
+	};
+	
+	/**
+	 * write a file with the specified lines
+	 * @param path
+	 * @param lines
+	 */
+	public void writeFile(String path, List<String> lines){
+		
+	}
+	
+	/**
+	 * Write just one line
+	 * @param path
+	 * @param line
+	 */
+	public void writeFile(String path, String line){
+		
+	}
+	
+}
