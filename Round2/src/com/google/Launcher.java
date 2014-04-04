2296f1ddeeecc978bfa6631396acdc7767d92a39
Julien Jacquemot <julien.jacquemot@gmail.com>
2014-04-04 22:49:48 +0200

    test

--- Round2/src/com/google/Launcher.java
+++ Round2/src/com/google/Launcher.java
@@ -7,7 +7,7 @@ public class Launcher {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("plop");
+		System.out.println("Hello World!");
 	}
 
 }
