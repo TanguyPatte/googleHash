46db01a0f82e355cefbcae2fcadced806dbd41c9
sam <samuel.humeau@epfl.ch>
2014-04-05 00:36:12 +0200

    iosimple

--- Round2/src/com/google/ClassBateauSam.java
+++
@@ -1,9 +0,0 @@
-package com.google;
-
-public class ClassBateauSam {
-
-	public static void main(String[] args) {
-		System.out.println("bateautesthhh");
-	}
-
-}
--- Round2/src/com/google/IOSimple.java
+++ Round2/src/com/google/IOSimple.java
@@ -1,15 +1,36 @@
 package com.google;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.LinkedList;
 import java.util.List;
 
-public class IOSimple {
+abstract  class IOSimple {
 	/**
 	 * Return a list of all lines in the file
 	 * @param path
 	 * @return
 	 */
-	List<String> readFile(String path){
-		return null;
+	public static List<String> readFile(String path){
+		LinkedList<String> list = null;
+		try{
+		FileReader fr=new FileReader(path); 
+		BufferedReader br=new BufferedReader(fr);
+		String ligne;
+		list= new LinkedList<String>();
+		while ((ligne=br.readLine())!=null){
+			if (ligne!="") // does not take into account empty line
+				list.add(ligne);
+		}
+		br.close();
+		fr.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return list;
 	};
 	
 	/**
@@ -17,17 +38,40 @@ public class IOSimple {
 	 * @param path
 	 * @param lines
 	 */
-	public void writeFile(String path, List<String> lines){
-		
+	public static void writeFile(String fileName, List<String> lines){
+		try{
+		FileWriter fw=new FileWriter(fileName);
+		BufferedWriter bw= new BufferedWriter(fw, 8192);
+		for (String s2 : lines){
+			bw.write(s2);
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+		fw.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	/**
-	 * Write just one line
+	 * Write just one string
 	 * @param path
 	 * @param line
 	 */
-	public void writeFile(String path, String line){
-		
+	public static void writeFile(String fileName, String line){
+		try{
+			FileWriter fw=new FileWriter(fileName);
+			BufferedWriter bw= new BufferedWriter(fw, 8192);
+			bw.write(line);
+			bw.flush();
+			bw.close();
+			fw.close();
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
 	}
 	
 }
