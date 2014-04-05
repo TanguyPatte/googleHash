217f55217b2152ca8ec4dff5edb44838dd074052
Tanguy PATTE <tanguy.patte@gmail.com>
2014-04-05 11:47:36 +0200

    add

--- Round2/src/com/google/Launcher.java
+++ Round2/src/com/google/Launcher.java
@@ -9,22 +9,7 @@ public class Launcher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<String> l = IOSimple.readFile("paris_54000.txt");
-		System.out.println(l.get(0));
-		String p[] = l.get(0).split(" ");
-		int initValues[] = new int[p.length];
-		int nbNode = Integer.parseInt(p[0]) ;
-		int nbEdge = Integer.parseInt(p[1]) ;
-		int time = Integer.parseInt(p[2]) ;
-		int nbCars = Integer.parseInt(p[3]) ;
-		int firstNode = Integer.parseInt(p[4]) ;
-		int index = 1;
-		Node.nodes = new Node[nbNode];
 		
-		while(index < nbNode){
-			String tmp[] = l.get(index).split(" ");
-			
-		}
 		
 		
 	}
---
+++ Round2/src/com/google/Reader.java
@@ -0,0 +1,27 @@
+package com.google;
+
+import java.util.List;
+
+public class Reader {
+
+	
+	public static void readFile(){
+		List<String> l = IOSimple.readFile("paris_54000.txt");
+		System.out.println(l.get(0));
+		String p[] = l.get(0).split(" ");
+		int initValues[] = new int[p.length];
+		int nbNode = Integer.parseInt(p[0]) ;
+		int nbEdge = Integer.parseInt(p[1]) ;
+		int time = Integer.parseInt(p[2]) ;
+		int nbCars = Integer.parseInt(p[3]) ;
+		int firstNode = Integer.parseInt(p[4]) ;
+		int index = 1;
+		Node.nodes = new Node[nbNode];
+		
+		while(index < nbNode){
+			String tmp[] = l.get(index).split(" ");
+			
+		}
+		
+	}
+}
