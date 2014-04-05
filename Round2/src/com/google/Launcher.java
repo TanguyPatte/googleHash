9ed310676d1d33691ee70dac8ca0486b0a527e97
sam <samuel.humeau@epfl.ch>
2014-04-05 12:03:32 +0200

    writingmarche

--- Round2/src/com/google/Launcher.java
+++ Round2/src/com/google/Launcher.java
@@ -9,7 +9,22 @@ public class Launcher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		Test1();
+	}
+	
+	/**
+	 * Test basic writing and reading
+	 * 
+	 */
+	public static void Test1(){
+		// create a fake array of vehicles
+		List<LinkedList<Node>> fake_solution= new LinkedList<LinkedList<Node>>();
+		for (int i=0; i<8; i++){
+			LinkedList<Node> test=new LinkedList<Node>();
+			test.add(new Node(10, 3,3));
+			fake_solution.add(test);
+		}
+		Writing.writeSolution(fake_solution);
 	}
 
 }
--- Round2/src/com/google/Writing.java
+++ Round2/src/com/google/Writing.java
@@ -5,16 +5,16 @@ import java.util.List;
 
 
 public class Writing {
-	public static void writeSolution(List<Node>[] result){
+	public static void writeSolution(List<LinkedList<Node>> result){
 		LinkedList<String> outputString=new LinkedList<String>();
-		outputString.add(String.valueOf(result.length));
-		for (int i=0; i<result.length; i++){
-			outputString.add(String.valueOf(result[i].size()));
-			for (Node n : result[i]){
+		outputString.add(String.valueOf(result.size()));
+		for (List<Node> list_node : result){
+			outputString.add(String.valueOf(list_node.size()));
+			for (Node n : list_node){
 				outputString.add(String.valueOf(n.index));
 			}
 		}
 		
-		IOSimple.writeFile("result_"+String.valueOf(System.currentTimeMillis()), outputString);
+		IOSimple.writeFile("result_"+String.valueOf(System.currentTimeMillis())+".txt", outputString);
 	}
 }
