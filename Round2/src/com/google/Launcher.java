0314de34f88810be20db811777e0c990baab91b9
Tanguy PATTE <tanguy.patte@gmail.com>
2014-04-05 11:44:40 +0200

    launcher

--- Round2/src/com/google/Launcher.java
+++ Round2/src/com/google/Launcher.java
@@ -1,6 +1,7 @@
 package com.google;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Launcher {
 
@@ -8,17 +9,24 @@ public class Launcher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		IOSimple.readFile("doodle.txt");
+		List<String> l = IOSimple.readFile("paris_54000.txt");
+		System.out.println(l.get(0));
+		String p[] = l.get(0).split(" ");
+		int initValues[] = new int[p.length];
+		int nbNode = Integer.parseInt(p[0]) ;
+		int nbEdge = Integer.parseInt(p[1]) ;
+		int time = Integer.parseInt(p[2]) ;
+		int nbCars = Integer.parseInt(p[3]) ;
+		int firstNode = Integer.parseInt(p[4]) ;
+		int index = 1;
+		Node.nodes = new Node[nbNode];
 		
-		LinkedList<String> b= new LinkedList<String>();
-		b.add("blabla");
-		b.add("blibli");
-		IOSimple.writeFile("coucou2.txt", b);
-		IOSimple.writeFile("coucou.txt", "on est les champions");
-		System.out.println("Ah que coucou!");
-		for (String s : IOSimple.readFile("coucou3.txt")){
-			System.out.println(s);
+		while(index < nbNode){
+			String tmp[] = l.get(index).split(" ");
+			
 		}
+		
+		
 	}
 
 }
